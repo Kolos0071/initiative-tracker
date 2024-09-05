ec98038ef0c165653f7c6a2572a02fb04f26cf05
Roman Ivanov <disaster0071@yandex.ru>
2024-09-05 23:12:56 +0500

    fix

--- src/main/data-service.ts
+++ src/main/data-service.ts
@@ -1,36 +1,17 @@
 import { openSync, closeSync, writeSync, readFileSync } from 'fs'
-// import * as path from 'path'
-
 import remote from 'electron'
 
 const app = remote.app
 
-
-
 export default {
   getData: async () => {
-
-    console.log("---------------");
-    console.log(app.getPath('userData'))
-    console.log("---------------");
 
     const userDataPath = app.getPath('userData');
 
     let resp
-    const json = [
-      {
-        name: 'Chuchka',
-        age: 25
-      },
-      {
-        name: 'djopA',
-        age: 13
-      },
-      {
-        name: '123421SAASD',
-        age: 12
-      }
-    ]
+    const options = {
+
+    }
     let fd
 
     try {
@@ -38,7 +19,7 @@ export default {
     } catch (error) {
       try {
         fd = openSync(userDataPath + '/options.json', 'a')
-        writeSync(fd, JSON.stringify(json))
+        writeSync(fd, JSON.stringify(options))
       } catch (err) {
         console.log(err)
       } finally {
@@ -46,8 +27,6 @@ export default {
         resp = readFileSync(userDataPath + '/options.json', 'utf8')
       }
     }
-
-   
 
     return resp
   }
