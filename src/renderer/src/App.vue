9eaac4e2902f4751b9baeac888639d2439c09f7e
Roman Ivanov <disaster0071@yandex.ru>
2024-09-09 16:26:19 +0500

    interface fix

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <button class="option-button" @click="optionFormState.toggler">Options</button>
   <Form :is-open="optionFormState.isOpen" class="option-form" @close="optionFormState.toggler()">
-    <h1>OPTION FORM</h1>
+    <h1>Настройки</h1>
     <p>
-      Внимание! Любое несохраненное изменение опций будет сброшено после перезапуска приложения.
+      Внимание! Любое несохраненное изменение будет сброшено после перезапуска приложения.
     </p>
     <div class="option-form__container">
       <div>
@@ -18,59 +18,61 @@
         <span>-</span>
         <input type="number" v-model="optionFormState.hpMax">
       </div>
-      <div>
-        <h2>HP appearance</h2>
-      </div>
     </div>
     <button @click="saveOptions()">Save</button>
   </Form>
   <Form class="fighter-form" :is-open="formState.isOpen" @close="formState.toggler()">
-    <ul class="fighter-form__input-list">
-      <li class="fighter-form__input-wrapper">
-        <label class="fighter-form__input-label" for="">Имя</label>
-        <input v-model="formState.name" type="text" />
-      </li>
-      <li class="fighter-form__input-wrapper">
-        <label class="fighter-form__input-label" for="">Инициатива</label>
-        <input v-model="formState.initiative" type="number" />
-      </li>
-      <li class="fighter-form__input-wrapper">
-        <label class="fighter-form__input-label" for="">Хиты</label>
-        <input v-model="formState.hits" type="number" />
-      </li>
-    </ul>
-    <div class="fighter-form__button-wrapper">
-      <button @click="formState.toggler()">Отмена</button>
-      <button @click="addFighter">Добавить</button>
-    </div>
+   <div class="fighter-form__wrapper">
+     <ul class="fighter-form__input-list">
+       <li class="fighter-form__input-wrapper">
+         <label class="fighter-form__input-label" for="">Имя</label>
+         <input class="fighter-form__input" v-model.trim="formState.name" type="text" />
+       </li>
+       <li class="fighter-form__input-wrapper">
+         <label class="fighter-form__input-label" for="">Инициатива</label>
+         <input class="fighter-form__input" v-model.number="formState.initiative" type="number" />
+       </li>
+       <li class="fighter-form__input-wrapper">
+         <label class="fighter-form__input-label" for="">Хиты</label>
+         <input class="fighter-form__input" v-model.number="formState.hits" type="number" />
+       </li>
+     </ul>
+     <div class="fighter-form__button-wrapper">
+       <button class="fighter-form__button" @click="formState.toggler()">Отмена</button>
+       <button class="fighter-form__button" @click="addFighter">Добавить</button>
+     </div>
+   </div>
   </Form>
   <h1>Раунд:{{ roundCounter }}</h1>
   <main>
     <div class="container">
       <div class="fight__add-button-wrapper">
         <button class="fight__add-button" @click="formState.toggler()">Добавить бойца</button>
-        <button class="fight__add-button" @click="addRandomFighter">Random</button>
+        <button class="fight__add-button" @click="addRandomFighter">Случайный боец</button>
       </div>
 
       <ul class="fighter-list">
         <li v-for="(fighter, id) in fightersList" :key="id" class="fighter-list__item-wrapper">
           <div
-            class="fighter-list__item"
+            class="fighter-list__item fighter"
           >
             <h2>{{ fighter.name }}</h2>
-            <h3>{{ fighter.initiative }}</h3>
+            <h3>Инициатива: {{ fighter.initiative }}</h3>
             <div v-if="fighter.hits > 0">
               <div>
-                <span>{{ fighter.hits }}</span
-                >/ <span>{{ fighter.maxHits }}</span>
+                <h4>HP:</h4>
+                <h3 class="fighter__hp" :class="{'fighter__hp_wounded':fighter.hits<fighter.maxHits}">
+                  <span>{{ fighter.hits }}</span
+                  >/<span>{{ fighter.maxHits }}</span>
+                </h3>
               </div>
-              <span v-for="(_, index) in fighter.hits" :key="index" class="fighter__hits" />
+<!--              <span v-for="(_, index) in fighter.hits" :key="index" class="fighter__hits" />-->
             </div>
-            <div v-if="fighter.hits > 0">
-              <input v-model="fighter.tempDamage" type="number" />
-              <button @click="dealDamage(id, fighter.tempDamage)">Нанести урон</button>
-              <button @click="healDamage(id, fighter.tempDamage)">{{fighter.hits>= fighter.maxHits?"Добавить":"Исцелить"}}</button>
-              <button :class="{ 'item_disabled': !fighter.isActive }"  @click="makeMove(id)">Ход</button>
+            <div class="fighter__handler-list" v-if="fighter.hits > 0">
+              <input class="fighter__handler fighter__input" v-model="fighter.tempDamage" type="number" />
+              <button class="fighter__handler fighter__button" @click="dealDamage(id, fighter.tempDamage)">Нанести урон</button>
+              <button class="fighter__handler fighter__button" @click="healDamage(id, fighter.tempDamage)">{{fighter.hits>= fighter.maxHits?"Добавить HP":"Исцелить"}}</button>
+              <button class="fighter__handler fighter__button" :class="{ 'item_disabled': !fighter.isActive }"  @click="makeMove(id)">Ход</button>
 
             </div>
             <div v-else>
